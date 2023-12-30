07d0c8faf0df9c3ac80cc4cbf561cae9848112c1
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-12-30 20:27:10 +0000

    Moved WizardGetSignInCodeByEmail and WizardSignInWithCode components

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -5,7 +5,9 @@ export { default as LogoAndControlsLayout } from './layouts/LogoAndControlsLayou
 export { default as HelpMessage } from './components/HelpMessage.vue'
 export { default as EmailInput } from './components/EmailInput.vue'
 export { default as CodeInput } from './components/CodeInput.vue'
-export { default as WizardGetSignInCodeByEmail } from './components/WizardGetSignInCodeByEmail.vue'
-export { default as WizardSignInWithCode } from './components/WizardSignInWithCode.vue'
 export { default as ErrorMessage } from './components/ErrorMessage.vue'
 export { default as SchoolOfDevotionLogo } from './components/SchoolOfDevotionLogo.vue'
+
+// containers
+export { default as WizardGetSignInCodeByEmail } from './containers/WizardGetSignInCodeByEmail.vue'
+export { default as WizardSignInWithCode } from './containers/WizardSignInWithCode.vue'
