7b1e2f2d2edd45ea80a1e5303342b035b7ce7071
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-01-11 07:49:33 +0000

    Add IonSkeletonText component to display loading animation for CachedImage.vue

--- src/shared/containers/CachedImage.vue
+++ src/shared/containers/CachedImage.vue
@@ -1,13 +1,19 @@
 <template>
+  <IonSkeletonText
+    v-if="downloader.isDownloading.value"
+    :animated="true"
+    class="img"
+  />
   <img
+    v-else
     :src="cachedUrl"
-    :class="{'cached' : cached}"
   >
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import { useDownloader } from '@/shared'
+import { IonSkeletonText } from '@ionic/vue'
 
 /* -------------------------------------------------------------------------- */
 /*                                  Interface                                 */
@@ -28,8 +34,7 @@ const downloader = useDownloader()
 /*                                    State                                   */
 /* -------------------------------------------------------------------------- */
 
-const cachedUrl = ref<string>('https://placehold.co/600x400/EEE/31343C')
-const cached = ref(false)
+const cachedUrl = ref<string>('')
 
 
 /* -------------------------------------------------------------------------- */
@@ -46,7 +51,6 @@ onMounted(onDownload)
 async function onDownload() {
   try {
     cachedUrl.value = await downloader.download(props.url)
-    cached.value = true
   } catch (err) {
     cachedUrl.value = props.url
     alert(err)
@@ -58,8 +62,6 @@ async function onDownload() {
 <style scoped>
 .img {
   height: 128px;
-}
-.cached {
-  border: 1px green solid;
+  margin: 0px;
 }
 </style>
