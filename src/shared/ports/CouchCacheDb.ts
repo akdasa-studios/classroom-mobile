3118320ba343dc33d86b8add223fc117139b500c
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-01-12 13:44:58 +0000

    use cordova-sqlite adapter on mobile devices

--- src/shared/ports/CouchCacheDb.ts
+++ src/shared/ports/CouchCacheDb.ts
@@ -1,3 +1,6 @@
+import { Capacitor } from '@capacitor/core'
 import { CouchDB } from './PouchDb/CouchDb'
 
-export const CouchCacheDb = new CouchDB('cache', {})
+export const CouchCacheDb = new CouchDB('cache', {
+  adapter: Capacitor.getPlatform() !== 'web' ? 'cordova-sqlite' : undefined,
+})
